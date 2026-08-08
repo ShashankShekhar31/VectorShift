d6afea2f30a9cc4507073faa1faa251fdf48e066
SHASHANK SHEKHAR <shashank.shekhar0331@gmail.com>
2026-08-08 13:27:23 +0530

    Fix CORS origins

--- backend/main.py
+++ backend/main.py
@@ -10,7 +10,7 @@ app = FastAPI()
 # Allow frontend (localhost:3000) to call this API
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:3000", "http://127.0.0.1:3000", "https://vector-shift-mu.vercel.app"],
+    allow_origins=["http://localhost:3000", "http://127.0.0.1:3000", "https://vector-shift-mu.vercel.app", "https://vector-shift-1tit5d4f2-shashank-shekhars-projects.vercel.app"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
