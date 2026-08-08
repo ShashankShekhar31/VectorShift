35a8fc88830a507249586ea7b39e31222c4edd77
SHASHANK SHEKHAR <shashank.shekhar0331@gmail.com>
2026-08-08 13:06:28 +0530

    add requirement

--- backend/main.py
+++ backend/main.py
@@ -10,7 +10,7 @@ app = FastAPI()
 # Allow frontend (localhost:3000) to call this API
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:3000", "http://127.0.0.1:3000"],
+    allow_origins=["http://localhost:3000", "http://127.0.0.1:3000", "https://vector-shift-5xff28cdy-shashank-shekhars-projects.vercel.app"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
